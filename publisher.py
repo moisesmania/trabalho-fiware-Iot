58a60e0b37466bdc443107b443d1c01e387f558d
Moisés José <121528693+moisesmania@users.noreply.github.com>
2026-06-14 20:17:00 +0000

    commit dos sensores funcionando 01/02

--- publisher.py
+++ publisher.py
@@ -11,13 +11,22 @@ client.loop_start()
 
 try:
     while True:
-        payload = {
+
+        sensor001 = {
             "temperature": round(20 + random.uniform(0, 10), 2),
             "humidity": round(50 + random.uniform(0, 20), 2)
         }
 
-        client.publish("/iot/sensor001", json.dumps(payload))
-        print("Publicado:", payload)
+        sensor002 = {
+            "co2": random.randint(350, 800),
+            "pm25": round(random.uniform(5, 30), 2)
+        }
+
+        client.publish("/iot/sensor001", json.dumps(sensor001))
+        client.publish("/iot/sensor002", json.dumps(sensor002))
+
+        print("Sensor001:", sensor001)
+        print("Sensor002:", sensor002)
 
         time.sleep(5)
 
